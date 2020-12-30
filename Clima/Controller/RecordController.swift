0a958b27bd9fbd025fbb479b89c2d70c3603cba0
hongjinseok <sldkdl111@ajou.ac.kr>
2020-12-31 02:35:37 +0900

    check and add RecordController

---
+++ Clima/Controller/RecordController.swift
@@ -0,0 +1,16 @@
+//
+//  RecordController.swift
+//  Clima
+//
+//  Created by 홍진석 on 2020/12/31.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecordContoller: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
